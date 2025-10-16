0c021677b01044700b8550d68434e59e502168a4
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-10-16 02:34:48 -0500

    side panel open a chat interface that was in popup. Context is added to handle the ai response waiting, use efftect are added to handle ai responses

--- src/sidepanel/App.jsx
+++ src/sidepanel/App.jsx
@@ -1,22 +1,88 @@
-import crxLogo from '@/assets/crx.svg'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '@/assets/vite.svg'
-import HelloWorld from '@/components/HelloWorld'
+import { useState, useRef, useEffect, createContext } from 'react';
+import ChatBox from '@/components/ChatBox'
 import './App.css'
 
+
+export const IsWaitingContext = createContext(null);
+
 export default function App() {
+  const defaultMessages = [
+    { message: "Hello!", messageClass: "received" }
+  ];
+
+  let [messages, setMessages] = useState(defaultMessages);
+  let [userMessageInput, setUserMessageInput] = useState("");
+  let [chatSession, setChatSession] = useState(undefined);
+  let lastMessageRef = useRef(null);
+  let [isWaitingForAi, setIsWaitingForAi] = useState(true);
+  let [aiResponse, setAiResponse] = useState("");
+
+  function messagesUpdate(newMessage = false, messageClass = "sent") {
+    let message = userMessageInput;
+    if (messageClass !== "sent") {
+      if (newMessage !== false) {
+        message = newMessage;
+      }
+      else {
+        message = "Error!! check console"
+        console.error("No message was received");
+      }
+    }
+    setMessages([...messages, { message: message, messageClass: messageClass }]);
+    if (messageClass === "sent") {
+      setUserMessageInput("");
+      setIsWaitingForAi(true);
+      sendUserPrompt(message)
+    }
+  }
+
+  function scrollToLastMessage() {
+    lastMessageRef.current?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+
+  async function initializeChatSession() {
+    const availability = await window.LanguageModel.availability();
+    if (availability === 'available') {
+      let session = await LanguageModel.create({
+        initialPrompts: [
+          { role: 'system', content: 'You are a helpful and friendly assistant.Be professional with responses. Do not use any emojis in response' }
+        ],
+      });
+      setChatSession(session);
+      setIsWaitingForAi(false);
+    }
+  }
+
+  async function sendUserPrompt(prompt) {
+    let ans = await chatSession.prompt([{ role: "user", content: prompt }]);
+    setIsWaitingForAi(false);
+    setAiResponse(ans);
+    console.log("AI Response: ", ans);
+    // messagesUpdate(ans, 'received');
+  }
+
+  useEffect(() => {
+    scrollToLastMessage();
+  }, [messages]);
+
+  useEffect(() => { initializeChatSession() },
+    []
+  )
+
+  useEffect(() => {
+    if (aiResponse !== "") {
+      messagesUpdate(aiResponse, 'received');
+      setAiResponse("");
+    }
+  },
+    [aiResponse]
+  )
+
   return (
-    <div>
-      <a href="https://vite.dev" target="_blank" rel="noreferrer">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-      <a href="https://crxjs.dev/vite-plugin" target="_blank" rel="noreferrer">
-        <img src={crxLogo} className="logo crx" alt="crx logo" />
-      </a>
-      <HelloWorld msg="Vite + React + CRXJS" />
-    </div>
+    <IsWaitingContext value={isWaitingForAi}>
+      <ChatBox messages={messages} lastMessageRef={lastMessageRef} messagesUpdate={messagesUpdate} userMessageInput={userMessageInput} setUserMessageInput={setUserMessageInput} />
+    </IsWaitingContext>
   )
 }
