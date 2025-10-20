39cb3582cf7050ca86ec97eda9bd89baeea5f60f
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-10-19 21:51:19 -0500

    moved css related to chat box from app.css to chatbox.css

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,13 +1,14 @@
 import Box from "@mui/material/Box";
 import ChatInput from "./ChatInput";
 import ChatMessageContainer from "./ChatMessageContainer";
+import './chatbox.css';
 
 
 export default function ChatBox(props) {
     // debugger;
     return (
         <Box className="chat-container">
-            <ChatMessageContainer messages={props.messages} lastMessageRef={props.lastMessageRef}/>
+            <ChatMessageContainer messages={props.messages} lastMessageRef={props.lastMessageRef} />
             <ChatInput messagesUpdate={props.messagesUpdate} userMessageInput={props.userMessageInput}
                 setUserMessageInput={props.setUserMessageInput} />
         </Box>
